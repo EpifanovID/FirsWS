b0a027a736f426b3e4e1d48bf607fe1b9bcd73ef
EpifanovID <144914985+EpifanovID@users.noreply.github.com>
2023-11-08 14:43:21 +0300

    Add files via upload

---
+++ js/description_of_the_algorithmic_task.js
@@ -0,0 +1,36 @@
+function solve() {
+    console.log("x, y, z");
+    let x = parseFloat(elementX.innerText);
+    let y = parseFloat(elementY.innerText);
+    let z = parseFloat(elementZ.innerText);
+    console.log(x, y, z)
+    if ((y + z <= x) || (x + z <= y) || (x + y <= z)) {
+        result = "Треугольник с сторонами X, Y, Z не существует.";
+         document.getElementById("result").innerText =  result;
+        return;
+    }
+    if ((x * x + y * y === z * z) || (y * y + z * z === x * x) || (z * z + x * x === y * y)) {
+        result = "Треугольник с сторонами X, Y, Z существует и он прямоугольний.";
+         document.getElementById("result").innerText =  result;
+    }
+    else {
+        result = "Треугольник с сторонами X, Y, Z существует, но он не прямоугольний.";
+         document.getElementById("result").innerText =  result;
+    }
+}
+function send() {
+    document.getElementById("UserEnter").submit();
+
+}
+function verify_send() {
+    solve();
+    send();
+}
+
+const elementX = document.getElementById("x");
+const elementY = document.getElementById("y");
+const elementZ = document.getElementById("z");
+
+const elementVerify = document.getElementById("solve");
+
+elementVerify.addEventListener('click', solve);
